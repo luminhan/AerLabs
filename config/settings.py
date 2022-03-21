2f66eff660e7098d9139c7a0575a865e31e76c50
Lu Min Han <lumin@obmn.nl>
2022-03-21 14:57:31 +0100

    add README

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h@*2fqjg5om(44d*&lqjb$)8)cxzm*4n-c^swc3%%mvzay0n5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
